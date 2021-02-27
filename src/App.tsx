224ef4a890b20143b209a7a5cd16d49402020094
Evan Daley <evandaley@Evans-MacBook-Pro.local>
2021-02-27 13:27:27 -0800

    Fixing eslint settings.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import icon from '../assets/icon.svg';
 import './App.global.css';
 
 const Hello = () => {
+  const writeLog = () => {
+    console.log("WE DID IT")
+  }
+
   return (
     <div>
       <div className="Hello">
@@ -11,30 +15,9 @@ const Hello = () => {
       </div>
       <h1>electron-react-boilerplate</h1>
       <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+        <button type="button" onClick={writeLog}>
+          Write a log
+        </button>
       </div>
     </div>
   );
