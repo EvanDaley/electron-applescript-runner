fa43139c2052a5a01f430fe3b74ada7b6ee9ae03
Evan Daley <evandaley@Evans-MacBook-Pro.local>
2021-02-28 14:33:27 -0800

    baby steps

--- src/App.tsx
+++ src/App.tsx
@@ -15,19 +15,23 @@ const ActionItemsList = ({ items = [{ name: ''}] }) => {
   return (
     <div>
       { items.map(function(item) {
-        // return <li key={item.name}>{item.name}</li>
         return <ActionItem key={item.name} item={item}/>
       })}
     </div>
-  );
+  )
 }
 
 const Main = () => {
   const runner = new Runner()
-  console.log(runner.scripts)
+
+  const handleKeyPress = (event) => {
+    if(event.key === 'Enter'){
+      console.log('enter press here! ')
+    }
+  }
 
   return (
-    <div>
+    <div onKeyPress={handleKeyPress}>
       <h1>Script Runner</h1>
       <div className="Main">
         <ActionItemsList items={runner.scripts}/>
