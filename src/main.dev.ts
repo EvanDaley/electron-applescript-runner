78dc1ef7f09030467e4199b8338fa9432393f663
Evan Daley <evandaley@corporatetools.com>
2021-06-22 12:34:44 -0700

    Tweaking some scripts

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -69,8 +69,9 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
+    width: 842,
+    height: 560,
+    // transparent: true,
     icon: getAssetPath('icon.png'),
     webPreferences: {
       nodeIntegration: true,
